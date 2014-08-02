2dc8f65c42c06a1e075d36a95ddbb9d28ed3c66c
Karuto Z <iamkaruto@gmail.com>
2014-08-01 20:05:50 -0700

    Added gridster

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(function(){ //DOM Ready
  
-    alert("Hello world!");
+    $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [140, 140]
+    });
  
 });
