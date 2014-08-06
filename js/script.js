c0a0c3801ef7e4c1fe38b56fa439b8bf63f82e6e
Karuto Z <iamkaruto@gmail.com>
2014-08-05 23:49:43 -0700

    Testing resize

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ $(function(){ //DOM Ready
  
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
+        widget_base_dimensions: [140, 140],
+		resize: {
+			enabled: true,
+			max_size: [4, 4],
+			min_size: [1, 1]
+		}
     });
  
 });
